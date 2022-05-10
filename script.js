ee10f0177daabe6e2d49c2bc027535e9517e8414
Shiva <shivakumarsunderamurthy@gmail.com>
2022-05-10 20:53:06 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let rock = document.getElementById('rock')
+let scissor = document.getElementById('scissor')
+let paper = document.getElementById("paper");
+
+
+
+function computerPlay(){
+    console.log('computer')
+}
+
+function playerSelection(){
+    console.log('test')
+}
+
+
+
+
+// function playRound(playerSelection, computerSelection) {
+//     playerSelection = playerSelection
+// }
+
+// const playerSelection = "rock";
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
