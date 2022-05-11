8cb4ba5125804bc11f3183b64c2f4f2ad575e40f
Shiva <shivakumarsunderamurthy@gmail.com>
2022-05-11 22:44:58 +0800

    Fixed: Loop to lower case input

--- script.js
+++ script.js
@@ -1,37 +1,62 @@
 choices = ['rock','paper','scissor']
-playerWon = 0
-computerWon = 0
-
-
+playerScore = 0
+computerScore = 0
 
 
 function game(){
-    // for (let i = 1; i < 5; i++) {
-    //   playRound(i);
-    // }
+  while(playerScore != 5 || computerScore != 5){
     playRound()
- }
+    if(playerScore == 5 || computerScore == 5){
+      break
+    }
+  }
+}
+
+// function game(){
+//     for (let i = 1; i < 5; i++) {
+//       playRound(i);
+//     }
+//     playRound()
+//  }
 
 function playRound(){
     const playerSelection = playerChoice()
-    const computerSelection = computerChoice() 
+    const computerSelection = computerChoice()
+    if (playerSelection == computerSelection) {
+      console.log("tie");
+    } else if (
+      (playerSelection == "rock" && computerSelection == "scissor") ||
+      (playerSelection == "scissor" && computerSelection == "paper") ||
+      (playerSelection == "paper" && computerSelection == "rock")
+    ) {
+       
+       playerScore++
+       console.log("player win: " + playerScore);
+    } else if (
+      (computerSelection == "rock" && playerSelection == "scissor") ||
+      (computerSelection == "scissor" && playerSelection == "paper") ||
+      (computerSelection == "paper" && playerSelection == "rock")
+    ) {
+       computerScore++;
+       console.log("computer win: " + computerScore);
+
+    }      
 }
   
 function playerChoice(){
     let input = prompt('Please type rock, paper, and scissor')
-    while (choices.includes(input) == false){ //if input is not equal to choices the boolean will return false thus executing the while loop
-        input = prompt('Please type rock, paper, and scissor')
-    }
+    while (input == null) {
+            input = prompt("Type Rock, Paper, or Scissors");
+ }
     input = input.toLowerCase()
-    // console.log(input)
-}
-
-function validateInput(choice){
-    if (choices.includes(choice)){
-        return true;
-    } else{
-        return false
-    }
+    while (choices.includes(input) == false){ //if input is not equal to choices the boolean will return false thus executing the while loop
+            input = prompt('Please type rock, paper, and scissor')
+    };
+    while (input == null) {
+      break
+    }     
+    input = input.toLowerCase()
+    return input  
 }
 
 function computerChoice(){
@@ -39,6 +64,5 @@ function computerChoice(){
 }
 
 
-game()
 
 
