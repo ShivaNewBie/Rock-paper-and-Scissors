8d360b925797d0c4576479a670e1de18bae14248
Shiva <shivakumarsunderamurthy@gmail.com>
2022-06-11 18:28:11 +0800

    fix cancel button infinite loop

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-choices = ['rock','paper','scissor']
-playerScore = 0
-computerScore = 0
+choices = ["rock", "paper", "scissor"];
+playerScore = 0;
+computerScore = 0;
 
+for (let i = 0; i < 5; i++) {
+  console.log(hello);
+}
 
-function game(){
-  while(playerScore != 5 || computerScore != 5){
-    playRound()
-    if(playerScore == 5 || computerScore == 5){
-      break
+function game() {
+  while (playerScore != 5 || computerScore != 5) {
+    playRound();
+    if (playerScore == 5 || computerScore == 5) {
+      break;
     }
   }
 }
@@ -19,50 +22,45 @@ function game(){
 //     playRound()
 //  }
 
-function playRound(){
-    const playerSelection = playerChoice()
-    const computerSelection = computerChoice()
-    if (playerSelection == computerSelection) {
-      console.log("tie");
-    } else if (
-      (playerSelection == "rock" && computerSelection == "scissor") ||
-      (playerSelection == "scissor" && computerSelection == "paper") ||
-      (playerSelection == "paper" && computerSelection == "rock")
-    ) {
-       
-       playerScore++
-       console.log("player win: " + playerScore);
-    } else if (
-      (computerSelection == "rock" && playerSelection == "scissor") ||
-      (computerSelection == "scissor" && playerSelection == "paper") ||
-      (computerSelection == "paper" && playerSelection == "rock")
-    ) {
-       computerScore++;
-       console.log("computer win: " + computerScore);
-
-    }      
-}
-  
-function playerChoice(){
-    let input = prompt('Please type rock, paper, and scissor')
-    while (input == null) {
-            input = prompt("Type Rock, Paper, or Scissors");
- }
-    input = input.toLowerCase()
-    while (choices.includes(input) == false){ //if input is not equal to choices the boolean will return false thus executing the while loop
-            input = prompt('Please type rock, paper, and scissor')
-    };
-    while (input == null) {
-      break
-    }     
-    input = input.toLowerCase()
-    return input  
+function playRound() {
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice();
+  if (playerSelection == computerSelection) {
+    console.log("tie");
+  } else if (
+    (playerSelection == "rock" && computerSelection == "scissor") ||
+    (playerSelection == "scissor" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock")
+  ) {
+    playerScore++;
+    console.log("player win: " + playerScore);
+  } else if (
+    (computerSelection == "rock" && playerSelection == "scissor") ||
+    (computerSelection == "scissor" && playerSelection == "paper") ||
+    (computerSelection == "paper" && playerSelection == "rock")
+  ) {
+    computerScore++;
+    console.log("computer win: " + computerScore);
+  }
 }
 
-function computerChoice(){
-    return choices[Math.floor(Math.random() * choices.length)]
+function playerChoice() {
+  let input = prompt("Please type rock, paper, and scissor");
+  //     while (input == null) {
+  //             input = prompt("Type Rock, Paper, or Scissors");
+  //  }
+  input = input.toLowerCase();
+  while (choices.includes(input) == false) {
+    //if input is not equal to choices the boolean will return false thus executing the while loop
+    input = prompt("Please type rock, paper, and scissor");
+  }
+  while (input == null) {
+    break;
+  }
+  input = input.toLowerCase();
+  return input;
 }
 
-
-
-
+function computerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
